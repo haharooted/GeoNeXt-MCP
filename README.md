083cea4af5bfebb4f9dd4fd9adbe48a3d109e030
Jonatan Katz <k3ring@protonmail.com>
2025-08-03 00:17:40 +0200

    FIx openai https mcp server

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GeoNeXt - MCP
+
+![Showcase](/pictures/Excalidraw_GEONEXTMCP_2.png)
+
+GeoNeXt is a MCP server built on fastmcp for enabling geolocation capability for LLMs
+
+
+
+Docs for fastmcp can be found here: https://gofastmcp.com/
+
--- geonext_mcp/server.py
+++ geonext_mcp/server.py
@@ -174,7 +174,7 @@ def _distance_between_coords(
 ###############################################################################
 def main() -> None:  # makes `python -m geonext_mcp` possible
     mcp.run(
-        transport="sse",          # Fast streaming HTTP transport
+        transport="sse",
         host=os.getenv("HOST", "0.0.0.0"),
         port=int(os.getenv("PORT", "8000")),
         log_level=os.getenv("UVICORN_LOG_LEVEL", "info"),
